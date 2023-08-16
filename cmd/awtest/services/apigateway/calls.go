f32237dffc93c43555d0b4e7a03e83e2ae3a4549
Matt Miller <matt@miller-media.com>
2023-08-16 01:08:23 -0700

    Added additional API Gateway calls

--- cmd/awtest/services/apigateway/calls.go
+++ cmd/awtest/services/apigateway/calls.go
@@ -9,11 +9,18 @@ import (
 	"github.com/aws/aws-sdk-go/service/apigateway"
 )
 
+type ApiWithStages struct {
+	Api    *apigateway.RestApi
+	Stages []*apigateway.Stage
+	Models []*apigateway.Model
+}
+
 var APIGatewayCalls = []types.AWSService{
 	{
 		Name: "apigateway:RestApis",
 		Call: func(sess *session.Session) (interface{}, error) {
-			var allApis []*apigateway.RestApi
+			var allApisWithStages []ApiWithStages
+
 			for _, region := range types.Regions {
 				regionSess, err := session.NewSession(&aws.Config{
 					Region: aws.String(region),
@@ -22,21 +29,43 @@ var APIGatewayCalls = []types.AWSService{
 					return nil, err
 				}
 				svc := apigateway.New(regionSess)
-				output, err := svc.GetRestApis(&apigateway.GetRestApisInput{})
+				apisOutput, err := svc.GetRestApis(&apigateway.GetRestApisInput{})
 				if err != nil {
 					return nil, err
 				}
-				allApis = append(allApis, output.Items...)
+				for _, api := range apisOutput.Items {
+					stagesOutput, err := svc.GetStages(&apigateway.GetStagesInput{RestApiId: api.Id})
+					if err != nil {
+						return nil, err
+					}
+					modelsOutput, err := svc.GetModels(&apigateway.GetModelsInput{RestApiId: api.Id})
+					if err != nil {
+						return nil, err
+					}
+					allApisWithStages = append(allApisWithStages, ApiWithStages{Api: api, Stages: stagesOutput.Item, Models: modelsOutput.Items})
+				}
 			}
-			return allApis, nil
+			return allApisWithStages, nil
 		},
 		Process: func(output interface{}, err error, debug bool) error {
 			if err != nil {
 				return utils.HandleAWSError(debug, "apigateway:RestApis", err)
 			}
-			if apis, ok := output.([]*apigateway.RestApi); ok {
-				for _, api := range apis {
-					utils.PrintResult(debug, "", "apigateway:RestApis", fmt.Sprintf("Found API Gateway: %s", *api.Name), nil)
+			if apisWithStages, ok := output.([]ApiWithStages); ok {
+				for _, apiWithStages := range apisWithStages {
+					apiName := *apiWithStages.Api.Name
+					if len(apiWithStages.Stages) == 0 {
+						utils.PrintResult(debug, "", "apigateway:GetStages", fmt.Sprintf("No stages found for API: %s, but access is granted.", apiName), nil)
+					} else {
+						for _, stage := range apiWithStages.Stages {
+							utils.PrintResult(debug, "", "apigateway:GetStages", fmt.Sprintf("Found Stage: %s (%s)", *stage.StageName, apiName), nil)
+						}
+					}
+					if len(apiWithStages.Models) > 0 {
+						for _, model := range apiWithStages.Models {
+							utils.PrintResult(debug, "", "apigateway:GetModels", fmt.Sprintf("Found Model: %s (%s)", *model.Name, apiName), nil)
+						}
+					}
 				}
 			}
 			return nil
@@ -74,6 +103,39 @@ var APIGatewayCalls = []types.AWSService{
 					for _, apiKey := range apiKeys {
 						utils.PrintResult(debug, "", "apigateway:GetApiKeys", fmt.Sprintf("Found API Key: %s", *apiKey.Id), nil)
 					}
+				}
+			}
+			return nil
+		},
+		ModuleName: types.DefaultModuleName,
+	},
+	{
+		Name: "apigateway:GetDomainNames",
+		Call: func(sess *session.Session) (interface{}, error) {
+			var allDomainNames []*apigateway.DomainName
+			for _, region := range types.Regions {
+				regionSess, err := session.NewSession(&aws.Config{
+					Region: aws.String(region),
+				})
+				if err != nil {
+					return nil, err
+				}
+				svc := apigateway.New(regionSess)
+				output, err := svc.GetDomainNames(&apigateway.GetDomainNamesInput{})
+				if err != nil {
+					return nil, err
+				}
+				allDomainNames = append(allDomainNames, output.Items...)
+			}
+			return allDomainNames, nil
+		},
+		Process: func(output interface{}, err error, debug bool) error {
+			if err != nil {
+				return utils.HandleAWSError(debug, "apigateway:GetDomainNames", err)
+			}
+			if domainNames, ok := output.([]*apigateway.DomainName); ok {
+				for _, domainName := range domainNames {
+					utils.PrintResult(debug, "", "apigateway:GetDomainNames", fmt.Sprintf("Found Domain Name: %s", *domainName.DomainName), nil)
 				}
 			}
 			return nil
